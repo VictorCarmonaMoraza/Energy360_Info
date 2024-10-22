50cda90413499fa5856619a0f0d422f3d9b88ea1
Victor <az_v.carmona.ext@W4ALL.SERVICES>
2024-10-22 21:46:38 +0200

    Creacion modelo Usuario

---
+++ Energy360_Info/Domain/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Energy360_Info.Domain.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName ="varchar(20)")]
+        public string NameUser { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(20)")]
+        public string Password { get; set; }
+    }
+}
