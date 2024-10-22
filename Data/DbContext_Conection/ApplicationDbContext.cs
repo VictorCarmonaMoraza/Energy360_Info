3d2157bb61da0c172ed444b83b299fa9b5759273
Victor <az_v.carmona.ext@W4ALL.SERVICES>
2024-10-22 22:04:40 +0200

    Instalacion de dependecias y creacion del ApplicationDbContext

---
+++ Data/DbContext_Conection/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Energy360_Info.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.DbContext_Conection;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+}
